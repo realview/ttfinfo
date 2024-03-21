d3355b0c1a6390a101714445c06110a4c0ebb30b
Derek <derek@realview.com.au>
2024-03-21 13:00:57 +1100

    types update

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare function ttfInfoFromPath(path: string):any;
-declare  function ttfInfoFromData(data:Buffer):any;
 
-export { ttfInfoFromPath,ttfInfoFromData };
+
+export { ttfInfoFromPath };
